472f0530bce8eef48f19d9b92643f787718aa130
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-04 21:10:14 +0200

    Add project files.

---
+++ 14. Extracting the right portion/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim MyText = "Visual Basic"
+        MyText = "Visual Basic"
+        MsgBox(Microsoft.VisualBasic.Right(MyText, 4))
+    End Sub
+End Class
